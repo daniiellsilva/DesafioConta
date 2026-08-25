c545426ad3af2d27aa39a1a2fd784598b6de6699
Daniel Silva <dsilvaoiliveira5@mail.com>
2026-08-25 09:46:41 -0300

    Primeiro commit do desafio java

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    static void main(String[] args) {
+        String nome = "Daniel";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("\n============================");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n============================");
+    }
+}
